732ce70488cd724ed274efb79cc0bb4a1eb620f3
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-17 23:47:40 +0100

    I modify the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import React from 'react'
+import Existing from '@components/Existing'
 import Layout from 'src/layout'
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+      <Existing />
     </Layout>
   )
 }
