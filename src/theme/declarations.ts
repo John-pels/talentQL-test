269a55c4fdff373a16507ffb640688649027de27
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-17 15:30:10 +0100

    I create src and move pages and styles folder into it.

---
+++ src/theme/declarations.ts
@@ -0,0 +1,88 @@
+/**
+ * colors defined here
+ */
+
+const colors = {
+  primary: '',
+  secondary: '',
+  default: '#FFF',
+}
+
+/**
+ * @param maxWidth max width of media query
+ */
+
+const customMediaQuery = (maxWidth: number) => `@media (max-width: ${maxWidth}px)`
+/**
+ * List of available media queries
+ * To use media query from a styled components, use
+ * ```js
+ * ${({theme}) => theme.media.desktop}
+ * ```
+ */
+
+interface Media {
+  custom: (maxNumber: number) => string
+  sm: (maxNumber: number) => string
+  md: string
+  lg: string
+  xl: string
+}
+
+const media: Media = {
+  custom: customMediaQuery,
+  /**
+   * small displays like phones
+   */
+  sm: customMediaQuery,
+  /**
+   * medium displays like tablets
+   */
+  md: customMediaQuery(768),
+  /**
+   * fairly large displays like small laptops
+   */
+  lg: customMediaQuery(992),
+  /**
+   * laptops and others
+   */
+  xl: customMediaQuery(1024),
+}
+
+/**
+ *
+ * @param val font size as number(unitless)
+ */
+
+const customFontSize = (val: number) => `${val}px`
+
+const fontSize = {
+  custom: customFontSize,
+  small: customFontSize(12),
+  /**
+   * normal font size
+   */
+  normal: customFontSize(16),
+  /**
+   * font size for headers
+   */
+  heading: customFontSize(20),
+
+  hero: customFontSize(64),
+
+  navlink: customFontSize(14),
+}
+
+/**
+ * font family
+ */
+const fontFamily = {
+  /**
+   * normal font family
+   */
+  /**
+   * font family for headers
+   */
+}
+
+export { colors, media, fontSize, fontFamily }
---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+/**
+ * All global css stylings should be done on this file
+ */
+export const GlobalStyles = createGlobalStyle`
+    body{
+        background-color: #fff;
+    }
+`
