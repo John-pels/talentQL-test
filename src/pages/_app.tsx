56dc4f737b8bc186e56d81f22f79057333f0740c
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-17 23:46:14 +0100

    I wrap the entire app into the styled-component theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import '@styles/globals.css'
+import theme from '@theme/index'
+import { GlobalStyles } from '@theme/globalStyles'
+import '../theme/normalize.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
