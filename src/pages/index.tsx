ad1b7bbdd141a460e4ec67bcb4238f941762120f
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-18 00:15:27 +0100

    I add the Become component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Existing from '@components/Existing'
 import Layout from 'src/layout'
+import Become from '@components/Become'
 
 export default function Home() {
   return (
     <Layout title="Home">
       <Existing />
+      <Become />
     </Layout>
   )
 }
