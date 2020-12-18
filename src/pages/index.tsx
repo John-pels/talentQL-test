8a1aeaf1c54675b185f1343aac70427dc155526e
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-18 01:12:12 +0100

    I add header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Existing from '@components/Existing'
 import Layout from 'src/layout'
 import Become from '@components/Become'
+import Header from '@components/Header'
 
 export default function Home() {
   return (
     <Layout title="Home">
+      <Header />
       <Existing />
       <Become />
     </Layout>
