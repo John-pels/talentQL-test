cffe409beebb5fc0871c5fdeb87ac0e6b545207e
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-18 01:12:32 +0100

    I add the tags

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
+import { MarginalContainer } from '@components/container'
 import React from 'react'
-
+import { AiOutlineLeft } from 'react-icons/ai'
+import { HeaderContainer, HeroText, SubHeroText, TextWrapper, Button } from './style'
 const Header = () => {
-  return <div></div>
+  return (
+    <HeaderContainer>
+      <Button>
+        <AiOutlineLeft /> &nbsp;Back
+      </Button>
+      <MarginalContainer>
+        <TextWrapper>
+          <SubHeroText>Contact</SubHeroText>
+          <HeroText>MTN Suppliers</HeroText>
+        </TextWrapper>
+      </MarginalContainer>
+    </HeaderContainer>
+  )
 }
 
 export default Header
