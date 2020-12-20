a49f97695221e5d8889a771ac684b6e89318768e
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-20 04:23:31 +0100

    I update the file

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-export const BASE_URL = ''
+export const LIMIT = '&limit=30&offset=0&rating=G&lang=en'
+export const API_KEY = 'OYa73aJOBfbenMLiAav9ZXswodSdoTTW'
+export const BASE_URL = 'https://api.giphy.com/v1/gifs'
 
 const api = axios.create({
   baseURL: BASE_URL,
