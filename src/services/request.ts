115b90a4dde36c1dd358390d8a64bb3416533b08
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-20 04:23:56 +0100

    I update the search request

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,8 +1,11 @@
-import axios from 'axios'
-import api from './index'
+import axios, { AxiosResponse } from 'axios'
+import api, { LIMIT } from './index'
 
-export const searchRequest = async () => {
-  const response = await api.get('/faqs')
+const GET_GIPHY = (apiKey: string, keyword: string) =>
+  `/search?api_key=${apiKey}&q=${keyword}${LIMIT}`
+
+export const searchRequest = async (apiKey: string, keyword: string) => {
+  const response: AxiosResponse<any> = await api.get(GET_GIPHY(apiKey, keyword))
   return response.data.data
 }
 
