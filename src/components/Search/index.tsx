3a9380f20f9ea1987ba475560e794de8bce5922d
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-12-20 04:22:25 +0100

    I move the Modal out of the search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,14 +1,40 @@
-import { SearchContainer, SearchInput, SearchIcon, InputGroup, Form } from './style'
-const Search = () => {
+import CustomModal from '@components/Modal'
+import { API_KEY } from '@services/index'
+import { searchRequest } from '@services/request'
+import { FC, ChangeEvent, useState, FormEvent, Fragment } from 'react'
+import { SearchContainer, SearchInput, SearchIcon, InputGroup, Form, Title } from './style'
+import { SearchTypes } from './types'
+
+const Search: FC<SearchTypes> = ({ setGiphy, setShowModal }) => {
+  const [keyword, setKeyword] = useState<string>('')
+
+  const handlechange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target
+    setKeyword(value)
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    try {
+      const data: any = await searchRequest(API_KEY, keyword)
+      setGiphy(data)
+      setShowModal(true)
+    } catch (error) {
+      console.log(error)
+    } finally {
+    }
+  }
   return (
-    <SearchContainer>
-      <Form>
-        <InputGroup>
-          <SearchInput placeholder="Search..." />
-          <SearchIcon />
-        </InputGroup>
-      </Form>
-    </SearchContainer>
+    <Fragment>
+      <SearchContainer>
+        <Form role="search" onSubmit={handleSubmit}>
+          <InputGroup>
+            <SearchInput placeholder="Search..." onChange={handlechange} />
+            <SearchIcon />
+          </InputGroup>
+        </Form>
+      </SearchContainer>
+    </Fragment>
   )
 }
 
